95223c596a238517c733fbb44cc4ee9e133a3ba6
Hakan ÖZER <hakanozer02@gmail.com>
2026-03-10 11:59:04 +0300

    Days_2

--- app/src/main/java/com/works/MainActivity.kt
+++ app/src/main/java/com/works/MainActivity.kt
@@ -2,6 +2,8 @@ package com.works
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,15 +21,28 @@ import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var l_txtEmail: EditText
+    lateinit var l_txtPassword: EditText
+    lateinit var l_btnLogin: Button
+    lateinit var l_btnRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+
+        l_txtEmail = findViewById(R.id.l_txtEmail)
+        l_txtPassword = findViewById(R.id.l_txtPassword)
+        l_btnLogin = findViewById(R.id.l_btnLogin)
+        l_btnRegister = findViewById(R.id.l_btnRegister)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
 
     }
 
